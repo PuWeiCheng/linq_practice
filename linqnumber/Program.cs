1d9c5666ca8ca7869745fd432006f5c65e5a0241
Puweicheng <wayne0505tw@gmail.com>
2023-02-21 11:47:05 +0800

    加入專案檔案。

---
+++ linqnumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqnumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串以逗號分隔的字串:");
+            string input = Console.ReadLine();
+            string[] number = input.Split(',');
+            string[] reversed = number.Reverse().ToArray();
+            string answer = string.Join(",", reversed);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
---
+++ practice2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("輸入一串以逗號分隔的整數數字字串:");
+            string input = Console.ReadLine();
+            int[] num = input.Split(',').Select(int.Parse).ToArray();
+            var oddnum = num.Where((x) => x % 2 == 1).OrderBy((x) => x);
+            Console.WriteLine("奇數:" + string.Join(",", oddnum));
+
+            var evennum = num.Where((x) => x % 2 == 0).OrderBy((x) => x);
+            Console.WriteLine("偶數:" + string.Join(",", evennum));
+            Console.ReadLine();
+        }
+    }
+}
